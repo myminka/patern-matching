63a5e7a7d880a49805fb0703d46ce002abaeda39
Maxim Silkov <maxim.silkou.2002@gmail.com>
2021-11-13 15:13:01 +0300

    add TimeBand.cs

---
+++ TimeBand.cs
@@ -0,0 +1,28 @@
+﻿namespace TollCallculation
+{
+    /// <summary>
+    /// Represent parts of day.
+    /// </summary>
+    public enum TimeBand
+    {
+        /// <summary>
+        /// The morning rush.
+        /// </summary>
+        MorningRush,
+
+        /// <summary>
+        /// The daytime.
+        /// </summary>
+        Daytime,
+
+        /// <summary>
+        /// The evening rush.
+        /// </summary>
+        EveningRush,
+
+        /// <summary>
+        /// The overnight.
+        /// </summary>
+        Overnight,
+    }
+}
